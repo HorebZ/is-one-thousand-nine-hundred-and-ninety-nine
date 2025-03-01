7fdf7f77d7b0e567f53762ee52a699248bd649a6
Horebz <Horebz@users.noreply.github.com>
2025-03-01 21:40:27 +0100

    test: add type checks for isOneThousandNineHundredAndNinetyNine function

--- src/is-one-thousand-nine-hundred-and-ninety-nine.test.ts
+++ src/is-one-thousand-nine-hundred-and-ninety-nine.test.ts
@@ -16,3 +16,31 @@ Deno.test("should return false when the input is a non-integer number", () => {
   assertEquals(isOneThousandNineHundredAndNinetyNine(1999.5), false);
   assertEquals(isOneThousandNineHundredAndNinetyNine(1999.99), false);
 });
+
+Deno.test("should return false when the input is a string", () => {
+  // @ts-expect-error - We expect this to be false
+  assertEquals(isOneThousandNineHundredAndNinetyNine("1999"), false);
+  // @ts-expect-error - We expect this to be false
+  assertEquals(isOneThousandNineHundredAndNinetyNine("2000"), false);
+});
+
+Deno.test("should return false when the input is an object", () => {
+  // @ts-expect-error - We expect this to be false
+  assertEquals(isOneThousandNineHundredAndNinetyNine({}), false);
+});
+
+Deno.test("should return false when the input is an array", () => {
+  // @ts-expect-error - We expect this to be false
+  assertEquals(isOneThousandNineHundredAndNinetyNine([]), false);
+});
+
+Deno.test("should return false when the input is a boolean", () => {
+  // @ts-expect-error - We expect this to be false
+  assertEquals(isOneThousandNineHundredAndNinetyNine(true), false);
+});
+
+Deno.test("should return false when the input is a function", () => {
+  // @ts-expect-error - We expect this to be false
+  assertEquals(isOneThousandNineHundredAndNinetyNine(() => {}), false);
+});
+
--- src/is-one-thousand-nine-hundred-and-ninety-nine.ts
+++ src/is-one-thousand-nine-hundred-and-ninety-nine.ts
@@ -1,3 +1,8 @@
+/**
+ * Check if a number is equal to one thousand nine hundred and ninety-nine
+ * @param number - The number to check
+ * @returns True if the number is equal to one thousand nine hundred and ninety-nine, false otherwise
+ */
 export const isOneThousandNineHundredAndNinetyNine = (
   number: number,
 ): boolean => {
